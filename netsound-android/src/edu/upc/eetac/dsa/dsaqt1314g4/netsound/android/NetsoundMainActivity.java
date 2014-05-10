20a7e1ea6efd54b53fba838f2c43675ce1ac1adc
ajimenezherrero <ajimenezherrero@gmail.com>
2014-05-10 17:25:12 +0200

    Se ha creado el proyecto android

---
+++ netsound-android/src/edu/upc/eetac/dsa/dsaqt1314g4/netsound/android/NetsoundMainActivity.java
@@ -0,0 +1,15 @@
+package edu.upc.eetac.dsa.dsaqt1314g4.netsound.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NetsoundMainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
