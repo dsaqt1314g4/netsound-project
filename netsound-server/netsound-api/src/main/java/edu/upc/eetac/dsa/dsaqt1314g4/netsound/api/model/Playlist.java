bb0992fff92ddfbb1521adf80be10731bbb474c3
ernest <ernest_espinola@hotmail.com>
2014-05-20 20:25:02 +0200

    Commmit Playlist

--- netsound-server/netsound-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g4/netsound/api/model/Playlist.java
+++ netsound-server/netsound-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g4/netsound/api/model/Playlist.java
@@ -1,5 +1,15 @@
 package edu.upc.eetac.dsa.dsaqt1314g4.netsound.api.model;
+import java.util.List;
 
+import javax.ws.rs.core.Link;
+
+import org.glassfish.jersey.linking.InjectLink;
+import org.glassfish.jersey.linking.InjectLink.Style;
+import org.glassfish.jersey.linking.InjectLinks;
+import org.glassfish.jersey.linking.Binding;
+
+import edu.upc.eetac.dsa.dsaqt1314g4.netsound.api.MediaType;
+import edu.upc.eetac.dsa.dsaqt1314g4.netsound.api.StingResource;
 public class Playlist {
 
 }
