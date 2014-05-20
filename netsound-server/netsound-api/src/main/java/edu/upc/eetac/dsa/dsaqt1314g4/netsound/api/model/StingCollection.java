26cafdbdaf5cb23642b7feda56854c094d5d12b1
ferrandiaz <fdia13350@gmail.com>
2014-05-20 20:19:27 +0200

    User, UserCollection, Sting, StingCollection

--- netsound-server/netsound-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g4/netsound/api/model/StingCollection.java
+++ netsound-server/netsound-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g4/netsound/api/model/StingCollection.java
@@ -1,5 +1,54 @@
 package edu.upc.eetac.dsa.dsaqt1314g4.netsound.api.model;
 
-public class StingCollection {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.ws.rs.core.Link;
+
+public class StingCollection {
+	private List<Link> links;
+	private List<Sting> stings;
+	private long newestTimestamp;
+	private long oldestTimestamp;
+
+	public StingCollection() {
+		super();
+		stings = new ArrayList<>();
+	}
+
+	public List<Link> getLinks() {
+		return links;
+	}
+
+	public void setLinks(List<Link> links) {
+		this.links = links;
+	}
+
+	public List<Sting> getStings() {
+		return stings;
+	}
+
+	public void setStings(List<Sting> stings) {
+		this.stings = stings;
+	}
+
+	public long getNewestTimestamp() {
+		return newestTimestamp;
+	}
+
+	public void setNewestTimestamp(long newestTimestamp) {
+		this.newestTimestamp = newestTimestamp;
+	}
+
+	public long getOldestTimestamp() {
+		return oldestTimestamp;
+	}
+
+	public void setOldestTimestamp(long oldestTimestamp) {
+		this.oldestTimestamp = oldestTimestamp;
+	}
+	
+	public void addSting(Sting sting){
+		stings.add(sting);
+	}
 }
