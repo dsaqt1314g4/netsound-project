f3d55a18121e73de295ff4f52ce55b2dd7ffc094
ernest <ernest_espinola@hotmail.com>
2014-05-22 15:41:07 +0200

    Playlist, PlaylistCollection

--- netsound-server/netsound-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g4/netsound/api/model/Playlist.java
+++ netsound-server/netsound-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g4/netsound/api/model/Playlist.java
@@ -3,13 +3,66 @@ import java.util.List;
 
 import javax.ws.rs.core.Link;
 
-import org.glassfish.jersey.linking.InjectLink;
-import org.glassfish.jersey.linking.InjectLink.Style;
-import org.glassfish.jersey.linking.InjectLinks;
-import org.glassfish.jersey.linking.Binding;
 
-import edu.upc.eetac.dsa.dsaqt1314g4.netsound.api.MediaType;
-import edu.upc.eetac.dsa.dsaqt1314g4.netsound.api.StingResource;
 public class Playlist {
-
+	
+	private List<Link> links;
+	private String playlistid;
+	private String userid;
+	private String playlist_name;
+	private String description;
+	private String style;
+	private long lastModified;
+	private String score;
+	
+	
+	public List<Link> getLinks() {
+		return links;
+	}
+	public void setLinks(List<Link> links) {
+		this.links = links;
+	}
+	public String getPlaylistid() {
+		return playlistid;
+	}
+	public void setPlaylistid(String playlistid) {
+		this.playlistid = playlistid;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getPlaylist_name() {
+		return playlist_name;
+	}
+	public void setPlaylist_name(String playlist_name) {
+		this.playlist_name = playlist_name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getStyle() {
+		return style;
+	}
+	public void setStyle(String style) {
+		this.style = style;
+	}
+	public long getLastModified() {
+		return lastModified;
+	}
+	public void setLastModified(long lastModified) {
+		this.lastModified = lastModified;
+	}
+	public String getScore() {
+		return score;
+	}
+	public void setScore(String score) {
+		this.score = score;
+	}
+	
 }
--- netsound-server/netsound-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g4/netsound/api/model/PlaylistCollection.java
+++ netsound-server/netsound-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g4/netsound/api/model/PlaylistCollection.java
@@ -1,5 +1,51 @@
 package edu.upc.eetac.dsa.dsaqt1314g4.netsound.api.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.core.Link;
+
 public class PlaylistCollection {
+	
+	private List<Link> links;
+	private List<Playlist> playlists;
+	private long newestTimestamp;
+	private long oldestTimestamp;
+	
+	public PlaylistCollection() {
+		super();
+		playlists = new ArrayList<>();
+	}
+	
+	public void addPlaylist(Playlist playlist) {
+		playlists.add(playlist);
+	}
+	
+	
+	public List<Link> getLinks() {
+		return links;
+	}
+	public void setLinks(List<Link> links) {
+		this.links = links;
+	}
+	public List<Playlist> getPlaylist() {
+		return playlists;
+	}
+	public void setPlaylist(List<Playlist> playlist) {
+		this.playlists = playlist;
+	}
+	public long getNewestTimestamp() {
+		return newestTimestamp;
+	}
+	public void setNewestTimestamp(long newestTimestamp) {
+		this.newestTimestamp = newestTimestamp;
+	}
+	public long getOldestTimestamp() {
+		return oldestTimestamp;
+	}
+	public void setOldestTimestamp(long oldestTimestamp) {
+		this.oldestTimestamp = oldestTimestamp;
+	}
+	
 
 }
