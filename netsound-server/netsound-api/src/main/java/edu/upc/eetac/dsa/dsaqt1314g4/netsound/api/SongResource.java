b6b41741c01cf47095734f405aa335eccab3cdb8
ajimenezherrero <ajimenezherrero@gmail.com>
2014-06-07 17:14:18 +0200

    Implementacion listado de canciones de un usuario y sistema de votaciones

--- netsound-server/netsound-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g4/netsound/api/SongResource.java
+++ netsound-server/netsound-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g4/netsound/api/SongResource.java
@@ -21,6 +21,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -48,7 +49,7 @@ public class SongResource {
 	@Produces(MediaType.NETSOUND_API_SONG_COLLECTION)
 	public SongCollection getSongs(@QueryParam("length") int length,
 			@QueryParam("before") long before, @QueryParam("after") long after,
-			@QueryParam("search") String search) {
+			@QueryParam("name") String name, @QueryParam("style") String style) {
 		SongCollection songs = new SongCollection();
 
 		Connection conn = null;
@@ -61,20 +62,30 @@ public class SongResource {
 		PreparedStatement stmt = null;
 		try {
 			boolean updateFromLast = after > 0;
-			boolean updateSearch = search != null;
-			stmt = conn.prepareStatement(buildGetSongsQuery(updateFromLast,
-					updateSearch));
-			if (updateSearch) {
-				stmt.setString(1, "%" + search + "%");
-			} else if (updateFromLast) {
-				stmt.setTimestamp(1, new Timestamp(after));
+			boolean updateName = name != null;
+			boolean updateStyle = style != null;
+			stmt = conn.prepareStatement(buildGetSongsQuery(updateFromLast));
+			if (updateName) {
+				stmt.setString(1, "%" + name + "%");
+			} else {
+				stmt.setString(1, "%%");
+			}
+
+			if (updateStyle) {
+				stmt.setString(2, "%" + style + "%");
+			} else {
+				stmt.setString(2, "%%");
+			}
+
+			if (updateFromLast) {
+				stmt.setTimestamp(3, new Timestamp(after));
 			} else {
 				if (before > 0)
-					stmt.setTimestamp(1, new Timestamp(before));
+					stmt.setTimestamp(3, new Timestamp(before));
 				else
-					stmt.setTimestamp(1, null);
+					stmt.setTimestamp(3, null);
 				length = (length <= 0) ? 20 : length;
-				stmt.setInt(2, length);
+				stmt.setInt(4, length);
 			}
 			ResultSet rs = stmt.executeQuery();
 			while (rs.next()) {
@@ -103,17 +114,80 @@ public class SongResource {
 		return songs;
 	}
 
-	private String buildGetSongsQuery(boolean updateFromLast,
-			boolean updateSearch) {
-		if (updateSearch)
-			return "select * from Songs where song_name like ? order by score desc";
-		else if (updateFromLast)
-			return "select * from Songs where last_modified > ? order by last_modified desc";
+	private String buildGetSongsQuery(boolean updateFromLast) {
+		if (updateFromLast)
+			return "select * from Songs where song_name like ? and style like ? and last_modified > ? order by last_modified desc";
 		else
-			return "select * from Songs where last_modified < ifnull(?, now())  order by last_modified desc limit ?";
+			return "select * from Songs where song_name like ? and style like ? and last_modified < ifnull(?, now())  order by last_modified desc limit ?";
 
 	}
 
+	@GET
+	@Path("/username/{username}")
+	@Produces(MediaType.NETSOUND_API_SONG_COLLECTION)
+	public SongCollection getSongsByUsername(
+			@PathParam("username") String username,
+			@QueryParam("length") int length,
+			@QueryParam("before") long before, @QueryParam("after") long after) {
+		SongCollection songs = new SongCollection();
+
+		Connection conn = null;
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		PreparedStatement stmt = null;
+		try {
+			boolean updateFromLast = after > 0;
+			stmt = conn.prepareStatement(buildGetSongsQueryByUsername(updateFromLast));
+			stmt.setString(1, username);
+			if (updateFromLast) {
+				stmt.setTimestamp(2, new Timestamp(after));
+			} else {
+				if (before > 0)
+					stmt.setTimestamp(2, new Timestamp(before));
+				else
+					stmt.setTimestamp(2, null);
+				length = (length <= 0) ? 20 : length;
+				stmt.setInt(3, length);
+			}
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				Song song = new Song();
+				song.setSongid(rs.getString("songid"));
+				song.setUsername(rs.getString("username"));
+				song.setSong_name(rs.getString("song_name"));
+				song.setAlbum(rs.getString("album_name"));
+				song.setDescription(rs.getString("description"));
+				song.setStyle(rs.getString("style"));
+				song.setDate(rs.getTimestamp("last_modified").getTime());
+				song.setScore(rs.getString("score"));
+				songs.addSong(song);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+			}
+		}
+
+		return songs;
+	}
+
+	private String buildGetSongsQueryByUsername(boolean updateFromLast) {
+		if (updateFromLast)
+			return "select * from Songs where username = ? and last_modified > ? order by last_modified desc";
+		else
+			return "select * from Songs where username = ? and last_modified < ifnull(?, now())  order by last_modified desc limit ?";
+
+	}
+	
 	@GET
 	@Path("/{songid}")
 	@Produces(MediaType.NETSOUND_API_SONG)
@@ -200,7 +274,7 @@ public class SongResource {
 							"insert into Songs (songid,username, song_name, album_name, description, style, score, num_votes) value (?,?,?,?,?,?,?,?)",
 							Statement.RETURN_GENERATED_KEYS);
 			stmt.setString(1, uuid.toString());
-			stmt.setString(2, security.getUserPrincipal().getName());
+			stmt.setString(2, "alejandro.jimenez");// System.out.println(security.getUserPrincipal().getName());
 			stmt.setString(3, song.getSong_name());
 			stmt.setString(4, song.getAlbum());
 			stmt.setString(5, song.getDescription());
@@ -258,8 +332,78 @@ public class SongResource {
 		return uuid;
 	}
 
+	@PUT
+	@Path("/{songid}")
+	@Consumes(MediaType.NETSOUND_API_SONG)
+	@Produces(MediaType.NETSOUND_API_SONG)
+	public Song updateSong(@PathParam("songid") String songid, Song song,
+			@FormDataParam("score") int score) {
+		// validateUser(stingid);
+		// validateUpdateSting(sting);
+		Connection conn = null;
+		int votantes;
+		double res_score;
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			throw new ServerErrorException("Could not connect to the database",
+					Response.Status.SERVICE_UNAVAILABLE);
+		}
 
-	
-	
-	
+		PreparedStatement stmt = null;
+		try {
+			stmt = conn.prepareStatement(buildGetSongById());
+			stmt.setInt(1, Integer.valueOf(songid));
+			ResultSet rs = stmt.executeQuery();
+
+			if (rs.next()) {
+				song.setSongid(rs.getString("songid"));
+				song.setUsername(rs.getString("username"));
+				song.setSong_name(rs.getString("song_name"));
+				song.setAlbum(rs.getString("album_name"));
+				song.setDescription(rs.getString("description"));
+				song.setStyle(rs.getString("style"));
+				song.setDate(rs.getTimestamp("last_modified").getTime());
+				song.setScore(rs.getString("score"));
+				song.setNum_votes(rs.getString("num_votes"));
+			} else {
+				throw new NotFoundException("There's no Song with songid="
+						+ songid);
+			}
+
+			votantes = Integer.valueOf(song.getNum_votes()) + 1;
+			res_score = (Integer.valueOf(song.getScore()
+					+ Integer.valueOf(score)))
+					/ votantes;
+			stmt = conn.prepareStatement(buildUpdateSting());
+			stmt.setDouble(1, res_score);
+			stmt.setInt(2, votantes);
+			stmt.setString(3, songid);
+			int rows = stmt.executeUpdate();
+
+			if (rows == 1)
+				song = getSongFromDatabase(songid);
+			else {
+				throw new NotFoundException("There's no sting with songid="
+						+ songid);
+			}
+
+		} catch (SQLException e) {
+			throw new ServerErrorException(e.getMessage(),
+					Response.Status.INTERNAL_SERVER_ERROR);
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+			}
+		}
+
+		return song;
+	}
+
+	private String buildUpdateSting() {
+		return "update Songs set score= ?, num_votes = ? where stingid=?";
+	}
 }
