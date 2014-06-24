12e405da68272f4511722499e99c33c0ec8a00c4
ernest <ernest_espinola@hotmail.com>
2014-06-24 16:35:45 +0200

    Song Resource, per a que no peti Android

--- netsound-server/netsound-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g4/netsound/api/SongResource.java
+++ netsound-server/netsound-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g4/netsound/api/SongResource.java
@@ -2,7 +2,6 @@ package edu.upc.eetac.dsa.dsaqt1314g4.netsound.api;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -13,6 +12,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
+import java.util.Date;
 import java.util.UUID;
 
 import javax.sql.DataSource;
@@ -134,6 +134,74 @@ public class SongResource {
 
 	}
 
+	// Get de todas las canciones de quien Sigo
+	@Path("/following")
+	@GET
+	@Produces(MediaType.NETSOUND_API_SONG_COLLECTION)
+	public SongCollection getSongsFollowing(@QueryParam("length") int length,
+			@QueryParam("before") long before, @QueryParam("after") long after) {
+		SongCollection songs = new SongCollection();
+		Connection conn = null;
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			throw new ServerErrorException("Could not connect to the database",
+					Response.Status.SERVICE_UNAVAILABLE);
+		}
+
+		PreparedStatement stmt = null;
+		try {
+			boolean updateFromLast = after > 0;
+			stmt = conn.prepareStatement(buildGetSongsFollowingQuery(updateFromLast));
+			stmt.setString(1, security.getUserPrincipal().getName());
+			if (updateFromLast) {
+				stmt.setTimestamp(2, new Timestamp(after));
+			} else {
+				if (before > 0)
+					stmt.setTimestamp(2, new Timestamp(before));
+				else
+					stmt.setTimestamp(2, null);
+				length = (length <= 0) ? 20 : length;
+				stmt.setInt(3, length);
+			}
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				Song song = new Song();
+				song.setSongid(rs.getString("songid"));
+				song.setUsername(rs.getString("username"));
+				song.setSong_name(rs.getString("song_name"));
+				song.setAlbum(rs.getString("album_name"));
+				song.setDescription(rs.getString("description"));
+				song.setStyle(rs.getString("style"));
+				song.setDate(rs.getTimestamp("last_modified").getTime());
+				song.setScore(rs.getString("score"));
+				song.setSongURL(app.getProperties().get("SongBaseURL")
+						+ song.getSongid());
+				songs.addSong(song);
+			}
+		} catch (SQLException e) {
+			throw new ServerErrorException(e.getMessage(),
+					Response.Status.INTERNAL_SERVER_ERROR);
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+			}
+		}
+
+		return songs;
+	}
+
+	private String buildGetSongsFollowingQuery(boolean updateFromLast) {
+		if (updateFromLast)
+			return "select s.* from Songs s, Follow f where f.followername = ? and s.username = f.followingname and last_modified > ? order by last_modified desc";
+		else
+			return "select s.* from Songs s, Follow f where f.followername = ? and s.username = f.followingname and last_modified < ifnull(?, now())  order by last_modified desc limit ?";
+
+	}
+	
 	// Get de todas las canciones de un usuario
 	@GET
 	@Path("/username/{username}")
@@ -176,7 +244,7 @@ public class SongResource {
 				song.setAlbum(rs.getString("album_name"));
 				song.setDescription(rs.getString("description"));
 				song.setStyle(rs.getString("style"));
-				song.setDate(rs.getTimestamp("last_modified").getTime());
+				song.setDate(new Date().getTime());
 				song.setScore(rs.getString("score"));
 				song.setSongURL(app.getProperties().get("SongBaseURL")
 						+ song.getSongid());
@@ -262,7 +330,7 @@ public class SongResource {
 				song.setStyle(rs.getString("style"));
 				song.setDate(rs.getTimestamp("last_modified").getTime());
 				song.setScore(rs.getString("score"));
-				song.setSongURL(app.getProperties().get("imgBaseURL")
+				song.setSongURL(app.getProperties().get("SongBaseURL")
 						+ song.getSongid());
 			} else {
 				throw new NotFoundException("There's no Song with songid="
@@ -288,7 +356,8 @@ public class SongResource {
 		return "select * from Songs where songid = ?";
 	}
 
-	// Post de una cancion
+
+
 	@POST
 	@Produces(MediaType.NETSOUND_API_SONG)
 	public Song uploadSong(@FormDataParam("songfile") InputStream file,
@@ -332,45 +401,50 @@ public class SongResource {
 					Response.Status.INTERNAL_SERVER_ERROR);
 		} finally {
 			try {
-				if (stmt != null)
-					stmt.close();
-				conn.close();
+				conn = ds.getConnection();
 			} catch (SQLException e) {
+				throw new ServerErrorException("Could not connect to the database",
+						Response.Status.SERVICE_UNAVAILABLE);
 			}
 		}
-		return song;
-	}
 
-	private String buildPostSong() {
-		return "insert into Songs (songid,username, song_name, album_name, description, style, score, num_votes) value (?,?,?,?,?,?,?,?)";
-	}
-
-	private UUID writeSong(InputStream file) {
-		UUID uuid = UUID.randomUUID();
-		String filename = uuid.toString() + ".mp3";
-		DataInputStream dis = new DataInputStream(file);
-		try {
-			DataOutputStream dos = new DataOutputStream(new FileOutputStream(
-					app.getProperties().get("uploadFolder") + filename));
-
-			byte[] buffer = new byte[1024];
-			int readed = 0;
-			while ((readed = dis.read(buffer)) != -1) {
-				dos.write(buffer, 0, readed);
-			}
-
-			dis.close();
-			dos.close();
-		} catch (FileNotFoundException e1) {
-			throw new InternalServerErrorException(
-					"Something has been wrong when converting the file.");
-		} catch (IOException e) {
-			throw new InternalServerErrorException(
-					"Something has been wrong when converting the file.");
+			return song;
 		}
 
-		return uuid;
-	}
+		private String buildPostSong() {
+			return "insert into Songs (songid,username, song_name, album_name, description, style, score, num_votes) value (?,?,?,?,?,?,?,?)";
+		}
+
+		private UUID writeSong(InputStream file) {
+			UUID uuid = UUID.randomUUID();
+			String filename = uuid.toString() + ".mp3";
+
+			DataInputStream dis = new DataInputStream(file);
+			
+			try {
+
+				DataOutputStream dos = new DataOutputStream(new FileOutputStream(
+						app.getProperties().get("uploadFolder") + filename));
+				System.out.println(app.getProperties().get("uploadFolder") + filename);
+				byte[] buffer = new byte[1024];
+				int readed = 0;
+				while ((readed = dis.read(buffer)) != -1) {
+					dos.write(buffer, 0, readed);
+				}
+
+				dis.close();
+				dos.close();
+			} catch (FileNotFoundException e1) {
+				throw new InternalServerErrorException(
+						"Something has been wrong when converting the file.");
+			} catch (IOException e) {
+				throw new InternalServerErrorException(
+						"Something has been wrong when converting the file.");
+			}
+
+			System.out.println(uuid);
+			return uuid;
+		}
 
 	// Put para puntuar una cancion
 	@PUT
@@ -450,6 +524,53 @@ public class SongResource {
 		return "update Songs set score= ?, num_votes = ? where songid=?";
 	}
 
+	@DELETE
+	@Path("/{songid}")
+	public void deleteSong(@PathParam("songid") String songid) {
+		validateUserDelete(songid);
+		Connection conn = null;
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			throw new ServerErrorException("Could not connect to the database",
+					Response.Status.SERVICE_UNAVAILABLE);
+		}
+	
+		PreparedStatement stmt = null;
+		try {
+			String sql = buildDeleteSong();
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, songid);
+	
+			int rows = stmt.executeUpdate();
+			if (rows == 0)
+				throw new NotFoundException("There's no Song with songid="
+						+ songid);
+		} catch (SQLException e) {
+			throw new ServerErrorException(e.getMessage(),
+					Response.Status.INTERNAL_SERVER_ERROR);
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+			}
+		}
+	}
+	
+	private String buildDeleteSong() {
+		return "delete from Songs where songid=?";
+	}
+	
+	private void validateUserDelete(String songid) {
+		Song currentSong = getSongFromDatabase(songid);
+		if (!security.getUserPrincipal().getName()
+				.equals(currentSong.getUsername()))
+			throw new ForbiddenException(
+					"You are not allowed to modify this sting.");
+	}
+	
 	// STINGS DE SONG
 
 	// Get de todos los stings de una cancion
@@ -532,9 +653,14 @@ public class SongResource {
 		PreparedStatement stmt = null;
 		PreparedStatement stmt2 = null;
 		try {
-			stmt = conn.prepareStatement(buildInsertSting(),
-					Statement.RETURN_GENERATED_KEYS);
-			stmt.setString(1, security.getUserPrincipal().getName());
+			String sql = buildInsertSting();
+			stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			try{
+				stmt.setString(1, security.getUserPrincipal().getName());
+			}catch(NullPointerException e){
+				stmt.setString(1, sting.getUsername());
+			}
+			
 			stmt.setString(2, sting.getContent());
 			stmt.executeUpdate();
 			ResultSet rs = stmt.getGeneratedKeys();
@@ -548,6 +674,7 @@ public class SongResource {
 			stmt2 = conn.prepareStatement(buildInsertSongSting());
 			stmt2.setInt(1, Integer.valueOf(sting.getStingid()));
 			stmt2.setString(2, songid);
+			stmt2.executeUpdate();
 		} catch (SQLException e) {
 			throw new ServerErrorException(e.getMessage(),
 					Response.Status.INTERNAL_SERVER_ERROR);
@@ -598,6 +725,7 @@ public class SongResource {
 			ResultSet rs = stmt.executeQuery();
 			long oldestTimestamp = 0;
 			if (rs.next()) {
+				sting.setStingid(stingid);
 				sting.setUsername(rs.getString("username"));
 				sting.setContent(rs.getString("content"));
 				sting.setLastModified(rs.getTimestamp("last_modified")
